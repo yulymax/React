0feb9ab3de5b7428d8920f899cb5869a5dd654cd
Christian Nahuel Balsamo <puyo.julia418@gmail.com>
2022-05-09 22:55:35 -0300

    detalles de item

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
-import listaItems from "../../items ";
+import listaItems from "../../items";
 
 const ItemListContainer = ({ greeting }) => {
   const [items, setItems] = useState([]);
