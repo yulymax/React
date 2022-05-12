b6798e0ca8d91814fb360305fee646757ce1b573
yulymax <85801118+yulymax@users.noreply.github.com>
2022-05-12 17:48:23 -0300

    Counter (#6)

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
 
 const ItemDetail = ({ item }) => {
+  const [cantidadAgregada, setCantidadAgregada] = useState();
+
   function onAdd(valor) {
-    console.log(`Llame al onAdd con ${valor}`);
+    setCantidadAgregada(valor);
   }
 
   return (
@@ -12,7 +14,7 @@ const ItemDetail = ({ item }) => {
       <h2>{item.title}</h2>
       <p>{item.price}</p>
 
-      <ItemCount stock={5} initial={1} onAdd={onAdd} />
+      {!cantidadAgregada && <ItemCount stock={5} initial={1} onAdd={onAdd} />}
     </div>
   );
 };
